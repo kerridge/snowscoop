0db2444c5a27b8e7dbc7ff9095d81f13478cbc4d
Sam Kerridge <kerridge@Sams-MacBook-Pro.local>
2019-05-10 17:09:21 +1200

    added routes for easy navigation

---
+++ lib/util/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:snowscoop/views/home.dart';
+
+final routes = {
+  // '/': (BuildContext context) => new Splash(),
+  '/home': (BuildContext context) => new Home()
+};
