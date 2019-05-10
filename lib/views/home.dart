dc85d23f1e080b14ed9f56b194cdfefb726e809b
Sam Kerridge <kerridge@Sams-MacBook-Pro.local>
2019-05-10 18:40:08 +1200

    added loading modal

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,11 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:snowscoop/view-models/home-state.dart';
 import 'package:snowscoop/views/widgets/line-chart.dart';
 
+import 'package:snowscoop/util/graph-util.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
 class HomeView extends HomeState {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new SimpleLineChart.withSampleData(),
+
+      body: new ModalProgressHUD(
+        inAsyncCall: scraping,
+        child: new SimpleLineChart(
+          buildSeries(rain),
+          animate: false,
+        ),
+      )
+      
+       
     );
   }
 }
