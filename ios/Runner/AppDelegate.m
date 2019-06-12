d726f86d76caa0107eb4c0855be7df4b8ebe5206
gooro672 <gooro672@student.otago.ac.nz>
2019-06-12 20:30:39 +1200

    added google maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyC8fukbFpiQJ1eEUeTOx9uvMKyZmWTTxgk"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
