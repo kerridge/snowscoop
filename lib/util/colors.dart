c71ab14d1a6f538180980b0b24c67ae3a43cf06b
Sam Kerridge <sammykerridge@gmail.com>
2019-06-24 13:53:11 +1200

    added colors file and removed unused files

---
+++ lib/util/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+List<Color> graphColors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.purple,
+    Colors.cyan,
+    Colors.deepOrange,
+    Colors.lime,
+    Colors.indigo,
+    Colors.yellow,
+    Colors.pink,
+    Colors.teal,
+    Colors.grey,
+    Colors.black,
+  ];
